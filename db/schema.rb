dbc147465ac164383773d8bb877c512d919fad11
Patrick Liang <pl204511194@yahoo.com>
2022-03-14 23:41:37 -0400

    modified files

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_08_180026) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_03_08_180026) do
   create_table "game_modes", force: :cascade do |t|
     t.string "game_mode"
   end
