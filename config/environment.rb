2f9f0779790123a33bdc7e105c240a727c7ae39e
Patrick Liang <pl204511194@yahoo.com>
2022-03-21 22:35:09 -0400

    another commit...

--- config/environment.rb
+++ config/environment.rb
@@ -1,35 +1,35 @@
-require 'bundler/setup'
-Bundler.require
-configure :development do
- ENV['SINATRA_ENV'] ||= "development"
-require 'bundler/setup'
- Bundler.require(:default, ENV['SINATRA_ENV'])
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
- )
-end
+# require 'bundler/setup'
+# Bundler.require
+# configure :development do
+#  ENV['SINATRA_ENV'] ||= "development"
+# require 'bundler/setup'
+#  Bundler.require(:default, ENV['SINATRA_ENV'])
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+#  )
+# end
 
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+# configure :production do
+#  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
 
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
+#  ActiveRecord::Base.establish_connection(
+#    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#    :host     => db.host,
+#    :username => db.user,
+#    :password => db.password,
+#    :database => db.path[1..-1],
+#    :encoding => 'utf8'
+#  )
+# end
 
-# ENV["SINATRA_ENV"] ||= "development"
+ENV["SINATRA_ENV"] ||= "development"
 
-# require "dotenv/load"
-# require "bundler/setup"
-# Bundler.require(:default, ENV["SINATRA_ENV"])
+require "dotenv/load"
+require "bundler/setup"
+Bundler.require(:default, ENV["SINATRA_ENV"])
 
-# # set :database, {adapter: "sqlite3", database: "db/#{ENV["SINATRA_ENV"]}.sqlite"}
-# set :database_file, "./database.yml"
+# set :database, {adapter: "sqlite3", database: "db/#{ENV["SINATRA_ENV"]}.sqlite"}
+set :database_file, "./database.yml"
 
-# require_all "app"
+require_all "app"
